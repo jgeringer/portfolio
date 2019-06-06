f68ef479fd708cb079283b6588bf6ebbfb9b7cf0
Joseph Geringer <joegeringer@gmail.com>
2019-06-06 11:34:16 -0500

    update config

--- plugins/contentful.js
+++ plugins/contentful.js
@@ -1,6 +1,11 @@
+require('dotenv').config()
 const contentful = require('contentful')
 
-module.exports = contentful.createClient({
+const config = {
   space: process.env.CTF_SPACE_ID,
   accessToken: process.env.CTF_ACCESS_TOKEN
-})
+}
+
+const client = contentful.createClient(config)
+
+export default client
